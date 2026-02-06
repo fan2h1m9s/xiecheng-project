0bf5213db542f3c2a9b6c6dd47bb8069342d2505
wangxinhe <3081684580@xiaomi.com>
2026-02-06 16:11:37 +0800

    初始

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,30 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { Address } from '../entities/Address';
+import { User } from '../entities/User';
+import { Hotel } from '../entities/Hotel';
+import { RoomType } from '../entities/RoomType';
+import { Room } from '../entities/Room';
+import { Order } from '../entities/Order';
+import { OrderRoomRelation } from '../entities/OrderRoomRelation';
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'hotel',
+  entities: [
+    Address,
+    User,
+    Hotel,
+    RoomType,
+    Room,
+    Order,
+    OrderRoomRelation
+  ],
+  synchronize: false,
+  logging: true,
+};
+
+export const AppDataSource = new DataSource(typeOrmConfig);
